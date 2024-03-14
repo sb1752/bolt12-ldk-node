fb59d747c96007c85c565e2fc0efb2b9966a276c
Shaan Batra <sb1752@gmail.com>
2024-03-14 17:00:24 +0000

    debug payment pending

--- src/lib.rs
+++ src/lib.rs
@@ -11,45 +11,54 @@ use core::time::Duration;
 const DEBUG: bool = true;
 
 pub fn run() -> () {
-    let mut config = default_config();
-    config.network = Network::Signet;
-    let mut builder = Builder::from_config(config);
-    builder.set_esplora_server("https://mutinynet.com/api/".to_string());
-	builder.set_listening_addresses(vec![SocketAddress::TcpIpV4 {
+    let mut config_a = default_config();
+    config_a.network = Network::Signet;
+    config_a.storage_dir_path = "/tmp/ldk_node_a".to_string();
+    let mut builder_a = Builder::from_config(config_a);
+    builder_a.set_esplora_server("https://mutinynet.com/api/".to_string());
+	builder_a.set_listening_addresses(vec![SocketAddress::TcpIpV4 {
 		addr: [127, 0, 0, 1],
 		port: 9376
 	}]).unwrap();
-	let node_a = Arc::new(builder.build().unwrap());
-
+	let node_a = Arc::new(builder_a.build().unwrap());
     node_a.start().unwrap();
-    let event_node = Arc::clone(&node_a);
-    std::thread::spawn(move || loop {
-        let event = event_node.wait_next_event();
-        if DEBUG { println!("GOT NEW EVENT: {:?}", event); }
-        if DEBUG { println!("Channels: {:?}", event_node.list_channels()); }
-        if DEBUG { println!("Payments: {:?}", event_node.list_payments()); }
-        event_node.event_handled();
-    });
 
-    let mut config = default_config();
-    config.network = Network::Signet;
-    let mut builder = Builder::from_config(config);
-    builder.set_esplora_server("https://mutinynet.com/api/".to_string());
-	builder.set_listening_addresses(vec![SocketAddress::TcpIpV4 {
+    let mut config_b = default_config();
+    config_b.network = Network::Signet;
+    config_b.storage_dir_path = "/tmp/ldk_node_b".to_string();
+    let mut builder_b = Builder::from_config(config_b);
+    builder_b.set_esplora_server("https://mutinynet.com/api/".to_string());
+	builder_b.set_listening_addresses(vec![SocketAddress::TcpIpV4 {
 		addr: [127, 0, 0, 1],
 		port: 9377
 	}]).unwrap();
-    let node_b = Arc::new(builder.build().unwrap());
+    let node_b = Arc::new(builder_b.build().unwrap());
     node_b.start().unwrap();
 
-    if DEBUG { println!("Node ID: {}", node_a.node_id()); }
-	if DEBUG { println!("Node listening address: {:?}", node_a.listening_addresses()); }
+    println!("node a id: {:?}", node_a.node_id());
+    println!("node b id: {:?}", node_b.node_id());
     if DEBUG { println!("Node A Address: {}", node_a.onchain_payment().new_address().unwrap()); }
     if DEBUG { println!("Node B Address: {}", node_b.onchain_payment().new_address().unwrap()); }
-    if DEBUG { println!("Funds: {:?}", node_a.list_balances()); }
-    if DEBUG { println!("Channels: {:?}", node_a.list_channels()); }
 
-    node_a.connect_open_channel(
+    let event_node = Arc::clone(&node_a);
+    std::thread::spawn(move || loop {
+        let event = event_node.wait_next_event();
+        if DEBUG { println!("NODE A: {:?}", event); }
+        event_node.event_handled();
+    });
+
+    let event_node = Arc::clone(&node_b);
+    std::thread::spawn(move || loop {
+        let event = event_node.wait_next_event();
+        if DEBUG { println!("NODE B: {:?}", event); }
+        event_node.event_handled();
+    });
+
+    // if DEBUG { println!("Node ID: {}", node_a.node_id()); }
+	// if DEBUG { println!("Node listening address: {:?}", node_a.listening_addresses()); }
+    // if DEBUG { println!("Funds: {:?}", node_a.list_balances()); }
+
+    let channel_id = node_a.connect_open_channel(
         node_b.node_id(),
         node_b.listening_addresses().unwrap().first().unwrap().clone(),
         10_000,
@@ -57,7 +66,10 @@ pub fn run() -> () {
         None,
         true).unwrap();
 
-	let offer = node_a.bolt12_payment().receive(10_000, "testing").unwrap();
+    if DEBUG { println!("Channels: {:?}", node_a.list_channels()); }
+    if DEBUG { println!("Channel ID: {:?}", channel_id)};
+
+	let offer = node_b.bolt12_payment().receive(10_000, "testing").unwrap();
 	if DEBUG { println!("Node offer: {}", offer); }
 
     loop {
@@ -68,14 +80,17 @@ pub fn run() -> () {
 
         if input == "ok" {
             let payment_id = node_a.bolt12_payment().send(&offer, Some("TODO".to_string())).unwrap();
-            if DEBUG { println!("*** Payment id: {:?} ***", payment_id); }
-            println!("-- Thank you for your payment");
+            println!("Thank you for your payment");
+            println!("Payment ID: {:?}", payment_id);
+            println!("Payment Status: {:?}", node_a.list_payments_with_filter(|p| p.id == payment_id).first().unwrap().status);
+            println!("Node A balance: {:?}", node_a.list_balances().total_lightning_balance_sats);
         } else if input == "exit" {
             node_a.stop().unwrap();
             node_b.stop().unwrap();
             break;
         } else {
             println!("-- Since you are sick you don't get to pay"); 
+            println!("Node A balance: {:?}", node_a.list_balances().spendable_onchain_balance_sats);
         }
         thread::sleep(Duration::from_secs(5));
     }
