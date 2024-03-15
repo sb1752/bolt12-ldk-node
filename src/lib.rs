5a2248f96fa7e07ad786472cbad4c14111eca5c7
Shaan Batra <sb1752@gmail.com>
2024-03-15 12:16:50 +0000

    working and prettified project

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@ use ldk_node::bitcoin::Network;
 use std::thread;
 use core::time::Duration;
 use ldk_node::LogLevel;
+use ldk_node::Event;
 
 const DEBUG: bool = true;
+const FUND: bool = false;
 
 pub fn run() -> () {
     let mut config_a = default_config();
@@ -38,63 +40,97 @@ pub fn run() -> () {
     let node_b = Arc::new(builder_b.build().unwrap());
     node_b.start().unwrap();
 
-    println!("node a id: {:?}", node_a.node_id());
-    println!("node b id: {:?}", node_b.node_id());
-    if DEBUG { println!("Node A Address: {}", node_a.onchain_payment().new_address().unwrap()); }
-    if DEBUG { println!("Node B Address: {}", node_b.onchain_payment().new_address().unwrap()); }
-
-    let event_node = Arc::clone(&node_a);
-    std::thread::spawn(move || loop {
-        let event = event_node.wait_next_event();
-        if DEBUG { println!("NODE A: {:?}", event); }
-        event_node.event_handled();
-    });
-
-    let event_node = Arc::clone(&node_b);
-    std::thread::spawn(move || loop {
-        let event = event_node.wait_next_event();
-        if DEBUG { println!("NODE B: {:?}", event); }
-        event_node.event_handled();
-    });
-
-    std::thread::sleep(Duration::from_secs(5));
-
-    let _ = node_a.connect_open_channel(
-        node_b.node_id(),
-        node_b.listening_addresses().unwrap().first().unwrap().clone(),
-        10_000,
-        None,
-        None,
-        false).unwrap();
-
-    if DEBUG { println!("Channels: {:?}", node_a.list_channels()); }
+    println!("Node A Started: {:?}", node_a.node_id());
+    println!("Node B Started: {:?}", node_b.node_id());
+    println!("Node A Starting Balance: {}", node_a.list_balances().total_lightning_balance_sats);
     
-    let mut offer = node_b.bolt12_payment().receive(1_000_000, "testing").unwrap();
-    if DEBUG { println!("Node offer: {}", offer); }
+    if FUND {
+        println!("Node A Address: {}", node_a.onchain_payment().new_address().unwrap());
+        println!("Node B Address: {}", node_b.onchain_payment().new_address().unwrap());
+    } else {
+        let event_node = Arc::clone(&node_a);
+        std::thread::spawn(move || loop {
+            let event = event_node.wait_next_event();
+            let display = match event {
+                Event::ChannelPending { funding_txo, .. } => format!("Node A Channel Pending. Funding TXO: {}", funding_txo),
+                Event::ChannelReady { .. } => format!("Node A Channel Ready\n Node A Starting Balance: {}", event_node.list_balances().total_lightning_balance_sats),
+                Event::PaymentSuccessful { .. } => {
+                    let balance = event_node.list_balances().total_lightning_balance_sats;
+                    format!("Payment Successful\n New Balance: {}", balance)
+                },
+                _ => "".to_string()
+            };
+            println!("{}", display);
+            event_node.event_handled();
+        });
 
-    loop {
-        println!("> Are you ok this week? ");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        let input = input.trim().to_lowercase();
+        let event_node = Arc::clone(&node_b);
+        std::thread::spawn(move || loop {
+            let event = event_node.wait_next_event();
+            let display = match event {
+                Event::ChannelPending { funding_txo, .. } => format!("Node B Channel Pending. Funding TXO: {}", funding_txo),
+                Event::ChannelReady { .. } => format!("Node B Channel Ready"),
+                Event::PaymentSuccessful { .. } => format!("Payment Successful"),
+                _ => "".to_string()
+            };
+            println!("{}", display);
+            event_node.event_handled();
+        });
 
-        if input == "ok" {
-            let payment_id = node_a.bolt12_payment().send(&offer, Some("TODO".to_string())).unwrap();
-            println!("Thank you for your payment");
-            println!("Payment ID: {:?}", payment_id);
-            println!("Payment Status: {:?}", node_a.list_payments_with_filter(|p| p.id == payment_id).first().unwrap().status);
-            println!("Node A balance: {:?}", node_a.list_balances().total_lightning_balance_sats);
-        } else if input == "exit" {
-            node_a.stop().unwrap();
-            node_b.stop().unwrap();
-            break;
-        } else if input == "create offer" {
-            offer = node_b.bolt12_payment().receive(1_000_000, "testing").unwrap();
-            if DEBUG { println!("Node offer: {}", offer); }
+        std::thread::sleep(Duration::from_secs(5));
+
+        let channels = node_a.list_channels();
+        if channels.len() > 0 {
+            let channel = channels.first().unwrap();
+            if channel.is_usable {
+                println!("***Channel is ready***\n");
+            } else {
+                node_a.connect(node_b.node_id(), node_b.listening_addresses().unwrap().first().unwrap().clone(), false).unwrap();
+                println!("channel: {:?}", channel);
+                panic!("Channel not usable!");
+            }
         } else {
-            println!("-- Since you are sick you don't get to pay"); 
-            println!("Node A balance: {:?}", node_a.list_balances().total_lightning_balance_sats);
+            node_a.connect_open_channel(
+                node_b.node_id(),
+                node_b.listening_addresses().unwrap().first().unwrap().clone(),
+                10_000,
+                None,
+                None,
+                false).unwrap();
         }
-        thread::sleep(Duration::from_secs(5));
-    }
+
+        let mut offer = node_b.bolt12_payment().receive(100_000, "testing").unwrap();
+        println!("Doctor's Weekly Fee: {} sats", 100);
+        println!("Node B (Doctor) offer: {}\n", offer);
+
+        loop {
+            let channels = node_a.list_channels();
+            let channel = channels.first().unwrap();
+            if channel.is_usable {
+                println!("\n> Are you (Node A) ok this week? ");
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                let input = input.trim().to_lowercase();
+    
+                if input == "ok" {
+                    let payment_id = node_a.bolt12_payment().send(&offer, Some("TODO".to_string())).unwrap();
+                    println!("Thank you for your payment");
+                    println!("Payment Status: {:?}", node_a.list_payments_with_filter(|p| p.id == payment_id).first().unwrap().status);
+                } else if input == "exit" {
+                    node_a.stop().unwrap();
+                    node_b.stop().unwrap();
+                    break;
+                } else if input == "create offer" {
+                    offer = node_b.bolt12_payment().receive(100_000, "testing").unwrap();
+                    if DEBUG { println!("Node offer: {}", offer); }
+                } else {
+                    println!("-- Since you are sick you don't have to pay"); 
+                    println!("Node A balance: {:?}\n", node_a.list_balances().total_lightning_balance_sats);
+                }
+                thread::sleep(Duration::from_secs(5));
+            } else {
+                //
+            }            
+        }
+    }    
 }
